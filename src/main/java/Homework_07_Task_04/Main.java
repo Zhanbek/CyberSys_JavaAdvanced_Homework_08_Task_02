e2b8950bd1605993e0b5b46a74cdb0e32a7904de
Sarsenov <zhanbek.sarsenov@gmail.com>
2026-02-22 18:35:29 +0500

    Реалізує завдання

--- src/main/java/Homework_07_Task_04/Main.java
+++ src/main/java/Homework_07_Task_04/Main.java
@@ -1,6 +1,36 @@
 package Homework_07_Task_04;
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        Thread first = new Thread(() -> {
+            try {
+                System.out.println("Потік first працює");
+                Thread.sleep(8000);
+            } catch (InterruptedException e) {
+                System.out.println("Перерваний потік first");
+            }
+        }, "first");
 
+        Thread second = new Thread(() -> {
+            try {
+                System.out.println("Потік second працює");
+                Thread.sleep(4500);
+            } catch (InterruptedException e) {
+                System.out.println("Перерваний потік second");
+            }
+        }, "second");
+
+        System.out.println("Потік main працює");
+        Thread.sleep(5000);
+        first.start();
+        second.start();
+
+        // Ждём завершения потоков
+        first.join();
+        System.out.println("Завершення first");
+
+        second.join();
+        System.out.println("Завершення second");
+
+        System.out.println("Завершення main");
     }
 }
